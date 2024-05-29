75174be91ecc9c4cae645c9b2ee4c47e379396dc
Carl <cknutson12@gmail.com>
2024-05-28 23:05:06 -0500

    update add_transaction

--- ynab-relay/main.py
+++ ynab-relay/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import requests
 import sys
@@ -6,11 +7,7 @@ ynab_api_url = 'https://api.ynab.com/v1'
 ynab_token = os.environ['ynab_token']
 ynab_budget_name = os.environ['ynab_budget_name']
 
-def add_transaction(account, payee, amount):
-    payee = 'Walmart' # 
-    amount = 930
-    transaction_account = 'Credit - Discover - 2nd'
-
+def add_transaction(account_name, payee, amount):
     budgets = requests.get(url=f'{ynab_api_url}/budgets', params={'include_accounts': 'true'}, headers={'Authorization': f'Bearer {ynab_token}'}).json()['data']['budgets']
     budget_id = None
     account_id = None
@@ -18,26 +15,26 @@ def add_transaction(account, payee, amount):
         if budget['name'] == ynab_budget_name:
             budget_id = budget['id']
             for account in budget['accounts']:
-                if account['name'] == transaction_account:
+                if account['name'] == account_name:
                     account_id = account['id']
     
     if not account_id:
-        print(f'No account found with name {transaction_account}.')
+        print(f'No account found with name {account_name}.')
         return
 
     transaction = {
         'transaction': {
             'account_id': account_id,
-            'date': '2024-05-19',
-            'amount': 12330,
+            'date': f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}',
+            'amount': amount.replace('.', ''),
             'payee_name': payee,
             'cleared': 'uncleared',
             'approved': False,
         }
     }
 
-    transaction = requests.post(url=f'{ynab_api_url}/budgets/{budget_id}/transactions', json=transaction, headers={'Authorization': f'Bearer {ynab_token}'})
-    transaction.raise_for_status
+    response = requests.post(url=f'{ynab_api_url}/budgets/{budget_id}/transactions', json=transaction, headers={'Authorization': f'Bearer {ynab_token}'})
+    response.raise_for_status
 
 def get_accounts():
     accounts = []
@@ -73,7 +70,10 @@ def main():
         return
 
     if action == 'add_transaction':
-        add_transaction()
+        try:
+            add_transaction(account_name=sys.argv[2], payee=sys.argv[3], amount=sys.argv[4])
+        except IndexError:
+            print(f'Action: {action}, requires an account, payee, and amount')
     elif action == 'get_payees':
         get_payees()
     elif action == 'get_accounts':
