5523c9a975841a7b1c59ed689c28aaa6a51be276
Carl <cknutson12@gmail.com>
2024-06-20 23:30:12 -0500

    transactions should be debits

--- ynab-relay/main.py
+++ ynab-relay/main.py
@@ -26,7 +26,7 @@ def add_transaction(account_name, payee, amount):
         'transaction': {
             'account_id': account_id,
             'date': f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}',
-            'amount': int(float(amount) * 1000),
+            'amount': -abs(int(float(amount) * 1000)),
             'payee_name': payee,
             'cleared': 'uncleared',
             'approved': False,
